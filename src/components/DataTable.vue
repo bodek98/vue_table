3b90eec0f774482cb1fb67ec7b45096ad528cf02
bodek98 <bodzio.entire@gmail.com>
2023-02-18 19:30:34 +0100

    Add json file with data + setup json-server + fetch data

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -5,26 +5,67 @@
   <table>
     <thead>
       <tr>
-        <th>
-          {{ column }}
-        </th>
+        <th>column</th>
+        <th>column</th>
+        <th>column</th>
+        <th>column</th>
+        <th>column</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>
-          {{ rowItem }}
-        </td>
+      <tr v-for="(item, index) in items" :key="index">
+        <td>{{ index }}</td>
+        <td>{{ item.ID }}</td>
+        <td>{{ item.PRODUCENT }}</td>
+        <td>{{ item.MODEL }}</td>
+        <td>{{ item["S/N"] }}</td>
+        <!-- <td>{{ item.COMPONENTS }}</td> -->
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import { onMounted, ref } from "@vue/runtime-core";
+import axios from "axios";
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
+  },
+  setup() {
+    const baseUrl = ref("http://localhost:3000");
+    let urls = ref([]);
+    let items = ref([]);
+    onMounted(async () => {
+      let i = ref(0);
+      let keepGoing = true;
+      while (keepGoing) {
+        const url = `${baseUrl.value}/${i.value}`;
+        try {
+          await axios.head(url);
+          urls.value.push(url);
+          i.value++;
+        } catch (error) {
+          keepGoing = false;
+        }
+      }
+      const fetchData = async () => {
+        try {
+          const responses = await Promise.all(
+            urls.value.map((url) => axios.get(url))
+          );
+          const data = responses.map((response) => response.data);
+          items.value = data;
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchData();
+    });
+    return {
+      items,
+    };
   },
 };
 </script>
